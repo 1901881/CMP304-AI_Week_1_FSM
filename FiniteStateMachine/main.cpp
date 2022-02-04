ded0adf11469d9b386398aa50ea32d875ec00360
1901881 <1901881@uad.ac.uk>
2022-02-04 11:22:36 +0000

    Initial Setup
    Turned the escape key exit code into a function

--- FiniteStateMachine/main.cpp
+++ FiniteStateMachine/main.cpp
@@ -7,6 +7,8 @@ using namespace std;			// cout, endl
 using namespace this_thread;	// sleep_for
 using namespace chrono;			// seconds, milliseconds
 
+bool getEscKey();
+
 int main()
 {
 	bool program_running = true;
@@ -24,20 +26,25 @@ int main()
 		// Sleep the current thread for 1000 milliseconds. Can be repalce with seconds(1)
 		sleep_for(milliseconds(1000));
 
-		if (_kbhit())
-		{
-			char input_char = _getch();
-
-			// Check for ESC key. See table here http://www.asciitable.com/
-			if (input_char == 27)
-			{
-				program_running = false;
-			}
-		}
-
-	} while (program_running);
+	} while (!getEscKey());
 
 	cout << "Ending Finite State Machine" << endl;
 
 	return 0;
+}
+
+bool getEscKey()
+{
+	bool isKeyPressed = false;
+	if (_kbhit())
+	{
+		char input_char = _getch();
+
+		// Check for ESC key. See table here http://www.asciitable.com/
+		if (input_char == 27)
+		{
+			isKeyPressed = true;
+		}
+		return isKeyPressed;
+	}
 }
